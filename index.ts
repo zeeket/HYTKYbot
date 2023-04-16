c63f3055f180945a4fe5f4fc431268c38e8e3b86
zeeket <emil.heliste@helsinki.fi>
2023-04-16 22:41:33 +0300

    add group checking

--- index.ts
+++ index.ts
@@ -7,30 +7,71 @@ dotenv.config()
 
 const port = process.env.PORT || 3000
 
+const isMemberInChat = async (userId: number, groupChatId: number, bot: Telegraf): Promise<boolean> => {
+  console.log('Checking if user', userId, 'is in chat', groupChatId)
+  try {
+    const chatMember = await bot.telegram.getChatMember(groupChatId, userId);
+    // Check if the user is a member of the chat and has not been kicked/banned
+    return ['member', 'administrator', 'creator'].includes(chatMember.status);
+  } catch (error) {
+    // Handle errors, e.g. if the user is not in the chat or the bot does not have permission to access chat members
+    console.error(error);
+    return false;
+  }
+}
+
+const isMemberInAnyChat = async (userId: number, groupChatIds: number[], bot: Telegraf): Promise<boolean> => {
+  const allPromiseResults:boolean[] = await Promise.all(groupChatIds.map( (groupChatId) => {
+    return isMemberInChat(userId, groupChatId, bot)
+  }))
+  return allPromiseResults.some( (result) => result )
+}
+
+const isBotInAllGroups = async (groupChatIds: number[], bot:Telegraf): Promise<boolean> => {
+  const botId = await bot.telegram.getMe().then((botInfo) => botInfo.id);
+  const allPromiseResults:boolean[] = await Promise.all(groupChatIds.map( (groupChatId) => {
+    return (bot.telegram.getChatMember(groupChatId, botId) != null)
+  }))
+  return allPromiseResults.every( (result) => result )
+}
+
 if (process.env.TG_BOT_TOKEN) {
   const app = new Koa()
-
   app.use(koaBody())
 
-  app.use((ctx) => {
-    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`
+  if (!process.env.TG_ADMIN_GROUP_IDS) {
+    console.error('No admin group ID provided')
+    process.exit(1)
+ } else if (!process.env.TG_ACTIVE_GROUP_IDS) {
+    console.error('No active group ID provided')
+    process.exit(1)
+  }
+
+  const adminGroups = process.env.TG_ADMIN_GROUP_IDS.split(',').map(id => parseInt(id))
+  const activeGroups = process.env.TG_ACTIVE_GROUP_IDS.split(',').map(id => parseInt(id))
+  
+  const bot = new Telegraf(process.env.TG_BOT_TOKEN)
+  //todo check if bot is in all groups
+
+  app.use(async (ctx) => {
+    //console.log(ctx.request.body)
+    if (ctx.request.body && ctx.request.body.user) {
+      if (await isMemberInAnyChat(ctx.request.body.user, adminGroups, bot)) {
+        ctx.body = JSON.stringify({role: 'admin'})
+      } else if (await isMemberInAnyChat(ctx.request.body.user, activeGroups, bot)) {
+        ctx.body = JSON.stringify({role: 'active'})
+      } else {
+        //TODO nakki groups(?)
+      ctx.body = JSON.stringify({role: 'nakki'})
+      }
+    } else {
+      ctx.body = 'No user ID provided'
+    }
   })
-  /* const bot = new Telegraf(process.env.TG_BOT_TOKEN);
-
-  bot.start((ctx) => ctx.reply("Welcome"));
-  bot.help((ctx) => ctx.reply("Send me a sticker"));
-  bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-  bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-  bot.launch();
-
-  app.use(async (ctx, next) =>
-	(await bot.createWebhook({ domain: "hytky.kasv.io" }))(ctx.req, ctx.res, next),
-  );
 
   // Enable graceful stop
   process.once("SIGINT", () => bot.stop("SIGINT"));
-  process.once("SIGTERM", () => bot.stop("SIGTERM")); */
-
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
   app.listen(port, () => { console.log('Listening on port', port) })
 } else {
   console.error('No token provided')
